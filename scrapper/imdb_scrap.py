6bad187008964786f15777114b6fa40b89f53449
unknown <rahulcr2797@gmail.com>
2025-12-16 20:57:44 +0530

    scrapy code

--- scrapper/imdb_scrap.py
+++ scrapper/imdb_scrap.py
@@ -45,6 +45,8 @@ class SpiderIMDB(scrapy.Spider):
         options.add_argument("--disable-blink-features=AutomationControlled")
         options.add_argument("--window-size=1920,1080")
 
+        print("Triggering Selenium using browser")
+
         driver = webdriver.Chrome(options=options)
         driver.get("https://www.imdb.com/chart/top/")
         time.sleep(5)
